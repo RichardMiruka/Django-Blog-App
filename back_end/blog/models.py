b01275b82b7818931254e4a61f2dd25566eba769
RichardMiruka <richardonsare15@gmail.com>
2025-02-10 13:19:40 +0300

    fixed autopep8 errors

--- back_end/blog/models.py
+++ back_end/blog/models.py
@@ -1,27 +1,44 @@
 from django.db import models
-from django.conf import settings 
+from django.conf import settings
 
-# Create your models here.
+
 class Profile(models.Model):
+    """
+    Represents a user profile with additional information
+    such as website and bio.
+    """
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
-        on_delete=models.PROTECT,
-        )
-        website = models.URLField(blank=True)
-        bio = models.charField(max_length=240, blank=True)
+        on_delete=models.PROTECT
+    )
+    website = models.URLField(blank=True)
+    bio = models.CharField(max_length=240, blank=True)
 
-        def __str__(self):
-            return self.user.get_username()
+    def __str__(self):
+        """
+        Returns the username associated with this profile.
+        """
+        return self.user.get_username()
+
 
 class Tag(models.Model):
+    """
+    Represents a tag that can be associated with posts.
+    """
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
+        """
+        Returns the name of the tag.
+        """
         return self.name
 
-# ...
 
 class Post(models.Model):
+    """
+    Represents a blog post with metadata such as title, body,
+    and publication status.
+    """
     title = models.CharField(max_length=255, unique=True)
     subtitle = models.CharField(max_length=255, blank=True)
     slug = models.SlugField(max_length=255, unique=True)
@@ -37,4 +54,9 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-publish_date"]
-    
+
+    def __str__(self):
+        """
+        Returns the title of the post.
+        """
+        return self.title
