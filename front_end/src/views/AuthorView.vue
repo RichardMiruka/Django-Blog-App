10adc1351fba3a61e684468d3474987028408a53
RichardMiruka <richardonsare15@gmail.com>
2025-02-12 12:40:09 +0300

    Updated the Vue Components

--- front_end/src/views/AuthorView.vue
+++ front_end/src/views/AuthorView.vue
@@ -1,15 +1,36 @@
 <script setup>
 import PostList from "../components/PostList.vue";
+import { useRoute } from "vue-router";
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
 
-const { result, loading, error } = {
-  error: { message: "No connection to the GraphQL API yet." },
-};
+const route = useRoute();
+const username = route.params.username;
+const { result, loading, error } = useQuery(gql`
+        query {
+          authorByUsername(
+            username: "${username}"
+          ) {
+              website
+              bio
+              user {
+                firstName
+                lastName
+                username
+              }
+              postSet {
+                title
+                slug
+              }
+            }
+        }
+      `);
 </script>
 
 <template>
   <div v-if="loading">Loading...</div>
   <div v-else-if="error">{{ error.message }}</div>
-  <section v-else :set="author = result.authorByUsername">
+  <section v-else :set="(author = result.authorByUsername)">
     <h2>{{ author.user.username }}</h2>
     <template v-if="author.user.firstName && author.user.lastName">
       <h3>{{ author.user.firstName }} {{ author.user.lastName }}</h3>
--- front_end/src/views/PostView.vue
+++ front_end/src/views/PostView.vue
@@ -1,17 +1,44 @@
 <script setup>
 import AuthorLink from "../components/AuthorLink.vue";
+import { useRoute } from "vue-router";
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
 
 const dateFormatter = new Intl.DateTimeFormat("en-US", { dateStyle: "full" });
 const displayableDate = (date) => dateFormatter.format(new Date(date));
-const { result, loading, error } = {
-  error: { message: "No connection to the GraphQL API yet." },
-};
+const route = useRoute();
+const slug = route.params.slug;
+const { result, loading, error } = useQuery(gql`
+      query {
+        postBySlug(
+          slug: "${slug}"
+        ) {
+            title
+            subtitle
+            publishDate
+            published
+            metaDescription
+            slug
+            body
+            author {
+              user {
+                username
+                firstName
+                lastName
+              }
+            }
+            tags {
+              name
+            }
+        }
+      }
+    `);
 </script>
 
 <template>
   <div v-if="loading">Loading...</div>
   <div v-else-if="error" class="warn">{{ error.message }}</div>
-  <section v-else :set="post = result.postBySlug">
+  <section v-else :set="(post = result.postBySlug)">
     <h2>{{ post.title }}</h2>
     <h3>{{ post.subtitle }}</h3>
     <p>{{ post.metaDescription }}</p>
@@ -28,12 +55,14 @@ const { result, loading, error } = {
         </li>
       </ul>
     </aside>
-    <article>{{ post.body }}</article>
+    <article>
+      {{ post.body }}
+    </article>
   </section>
 </template>
 
 <style scoped>
-  h2 {
-    color: orange;
-  }
+h2 {
+  border-top: green;
+}
 </style>
